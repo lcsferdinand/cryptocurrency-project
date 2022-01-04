73eadf5ebfd2a5b50fbf609a1218b1b48e87c18c
robin viltoriano <robinviltoriano@robins-MacBook-Pro.local>
2022-01-04 14:45:03 +0700

    update model.py

--- module/model.py
+++ module/model.py
@@ -8,8 +8,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 
-class model:
-  def train_test_split(self, ret_data,test_ratio=0.25):
+from utils import *
+
+def train_test_split(self, ret_data,test_ratio=0.25):
     """
     ret_data: return of cryptocurrency data
     """
@@ -20,15 +21,15 @@ class model:
     self.X_val = ret_data.iloc[-n_test:-1]
     self.y_val = ret_data.shift(-1).iloc[-n_test:-1]
 
-  def calculate_aic(n, mse, num_params):
-	  aic = n * m.log(mse) + 2 * num_params
-	  return aic
+def calculate_aic(n, mse, num_params):
+    aic = n * m.log(mse) + 2 * num_params
+    return aic
 
-  def calculate_bic(n, mse, num_params):
-      bic = n * m.log(mse) + num_params * m.log(n)
-      return bic
+def calculate_bic(n, mse, num_params):
+    bic = n * m.log(mse) + num_params * m.log(n)
+    return bic
 
-  def model_validation(y_val,y_pred,desc_stat_svr=desc_stat_svr):
+def model_validation(y_val,y_pred,desc_stat=desc_stat_svr):
     mse = mean_squared_error(y_val,y_pred)
     mae = mean_absolute_error(y_val,y_pred)
     rmse = np.sqrt(mean_squared_error(y_val,y_pred))
@@ -42,13 +43,13 @@ class model:
     print(f'AIC {aic}')
     print(f'BIC {bic} \n')
 
-    if desc_stat_svr:
-      print('Descriptive Statistics of Population Data')
-      print(stat_desc(y_val),'\n')
-      print('Descriptive Statistics of Predicted Data')
-      print(stat_desc(y_pred),'\n')
+    if desc_stat:
+        print('Descriptive Statistics of Population Data')
+        print(stat_desc(y_val),'\n')
+        print('Descriptive Statistics of Predicted Data')
+        print(stat_desc(y_pred),'\n')
 
-  def svr_plot(y_val,y_pred,rv_name):
+def svr_plot(y_val,y_pred,rv_name):
     figure(figsize=(10, 6), dpi=80)
     plt.plot(y_val,label=f'Observed {rv_name}')
     plt.plot(pd.Series(y_pred, index=y_val.index),'r',ls='--',label=f'SVR forecasted {rv_name}');
@@ -58,7 +59,9 @@ class model:
     plt.legend(loc='upper right')
     plt.show()
 
-  def svr(self, coins, period, kernel, eps, C=3, gamma=3, degree=3,plot_svr=True,desc_stat_svr=True,rv='r'):
+class model:
+
+  def svr(self,ret_data, coins, period, kernel, eps, C=3, gamma=3, degree=3,split = False,test_ratio=0.25,plot_svr=True,desc_stat_svr=True,rv='r'):
     """
     kernel: svr kerenel
     eps: svr epsilon 
@@ -68,13 +71,23 @@ class model:
     plot_svr: if True then the plot will be shown
     rv: calculate return or volatiltiy, where r: return and v: volatiliy
     first run rv = 'r' to able to run rv = 'v'
+    split: True if you want to train test split
     """
+    if split == True:  
+        train_test_split(self, ret_data,test_ratio=test_ratio)
+    else:
+       self.ret_data = ret_data
+       self.X = ret_data.iloc[:-1]
+       self.y = ret_data.shift(-1)
+       self.X_val = X
+       self.y_val = y
+    
     if rv == 'r':
       ret_data = self.ret_data
-      X = self.X#.reshape(-1,1)
-      y = self.y#.reshape(-1,1)
-      X_val = self.X_val#.reshape(-1,1)
-      y_val = self.y_val#.reshape(-1,1)
+      X = self.X
+      y = self.y
+      X_val = self.X_val
+      y_val = self.y_val
       # regressor_r = SVR(kernel = kernel, C=C, epsilon=eps,gamma=gamma,degree=degree)
       # regressor_r.fit(X,y)
       # self.regressor = regressor_r
@@ -111,6 +124,7 @@ class model:
       rv_name = 'Return'
     elif rv == 'v':
       rv_name = 'Volatility'
+      
     print_msg_box(f'SVR {rv_name}\n -Coin Type: {coins} \n -Period: {period} \n -Kernel: {kernel}')
 
     #SVR Coefficients
