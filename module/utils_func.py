09ed658c5470751bf6c08e9cfcaa409d79ee84d5
robin viltoriano <robinviltoriano@robins-MacBook-Pro.local>
2022-01-08 22:09:19 +0700

    fix garch_df function on utils_func and delete garch_df function on data_prep

--- module/utils_func.py
+++ module/utils_func.py
@@ -23,57 +23,78 @@ def stat_desc(y_pred):
     print('Mean {}'.format(y_pred.mean()))
     print('STD {}'.format(y_pred.std()))
 
-def garch_df(df_sg,p,q,test_ratio=0.25): #make data frame base on GARCH(p,q)
-  # df_sg = pd.DataFrame(u2)
-  # df_sg.rename(columns={'Return':'u2'},inplace=True)
-  # df_sg['vol_prox']=(df['Return'].iloc[:-1]-df['Return'].iloc[:-1].mean())**2
+def garch_df(df,p,q,o,ratio=0.25): #make data frame base on GARCH(p,q)
 
   if p>q:
     #return
     for i in range(1,p):
-      df_sg['u2_shift'+str(i)] =  df_sg['u2'].shift(-i)
+      df['u_squared_shift'+str(i)] =  df['u_squared'].shift(-i)
 
     #volatility
     diff = p-q
     for i in range(p,diff-1,-1):
-      df_sg['vol_prox_shift'+str(i)]= df_sg['vol_prox'].shift(-i)
+      df['vol_prox_shift'+str(i)]= df['vol_prox'].shift(-i)
 
   elif p<q:
     #return
     diff = q-p
     for i in range(q-1,diff-1,-1):
-      df_sg['u2_shift'+str(i)] =  df_sg['u2'].shift(-i)
+      df['u_squared_shift'+str(i)] =  df['u_squared'].shift(-i)
 
     #volatility
     for i in range(q,0,-1):
-      df_sg['vol_prox_shift'+str(i)]= df_sg['vol_prox'].shift(-i)
+      df['vol_prox_shift'+str(i)]= df['vol_prox'].shift(-i)
       
   else:
     #return
     for i in range(1,p):
-      df_sg['u2_shift'+str(i)] =  df_sg['u2'].shift(-i)
+      df['u_squared_shift'+str(i)] =  df['u_squared'].shift(-i)
     #volatility
     for i in range(1,q+1):
-      df_sg['vol_prox_shift'+str(i)]= df_sg['vol_prox'].shift(-i)  
+      df['vol_prox_shift'+str(i)]= df['vol_prox'].shift(-i)  
 
-  df_sg = df_sg.iloc[:-max(p,q)]
-  df_sg = df_sg[sorted(df_sg.columns)]
+  df = df.iloc[:-max(p,q)]
+  df = df[sorted(df.columns)]
 
   if p<q:
-    selected_col = df_sg[sorted(df_sg.columns)].iloc[:,:p+1].iloc[:,-p:].columns #select return
-    selected_col= selected_col.append(df_sg[sorted(df_sg.columns)].iloc[:,-q-1:-1].columns) #select volatility
+    selected_col = df[sorted(df.columns)].iloc[:,:p+1].iloc[:,-p:].columns #select return
+    selected_col= selected_col.append(df[sorted(df.columns)].iloc[:,-q-1:-1].columns) #select volatility
   else:
-    selected_col = df_sg[sorted(df_sg.columns)].iloc[:,:p].iloc[:,-p:].columns #select return
-    selected_col= selected_col.append(df_sg[sorted(df_sg.columns)].iloc[:,-q-1:-1].columns) #select volatility
-    
-  n_test = m.floor(len(df_sg)*test_ratio)
-  # print('n_test: ',n_test)
-  X_sg = df_sg[selected_col].iloc[:-n_test]
-  y_sg = df_sg[df_sg.iloc[:,-1:].columns].iloc[:-n_test]
-  X_sg_val = df_sg[selected_col].iloc[-n_test:]
-  y_sg_val = df_sg[df_sg.iloc[:,-1:].columns].iloc[-n_test:]
+    selected_col = df[sorted(df.columns)].iloc[:,:p].iloc[:,-p:].columns #select return
+    selected_col= selected_col.append(df[sorted(df.columns)].iloc[:,-q-1:-1].columns) #select volatility
 
-  return X_sg,y_sg,X_sg_val,y_sg_val
+  if o>0:
+    df['I']=0
+    for i in range(len(self.btc['Return'])):
+      if self.btc['Return'][i] < 0:
+        df['I'][i] = 1
+      else:
+        df['I'][i]=0
+
+    for i in range(o):
+      df['I_multiplied_shift'+str(i)] = df['I']*df[selected_col[i]]
+      selected_col=np.append(selected_col,'I_multiplied_shift'+str(i))
+
+    df.drop('I',axis=1,inplace=True)
+    df.drop('Return',axis=1,inplace=True)
+
+    n_test = m.floor(len(df)*ratio)
+    X = df[selected_col].iloc[:-n_test]
+    y = df[df.iloc[:,-(1+o):-o].columns].iloc[:-n_test]
+    X_val = df[selected_col].iloc[-n_test:]
+    y_val = df[df.iloc[:,-(1+o):-o].columns].iloc[-n_test:]
+
+  else:
+    n_test = m.floor(len(df)*ratio)
+    X = df[selected_col].iloc[:-n_test]
+    y = df[df.iloc[:,-1:].columns].iloc[:-n_test]
+    X_val = df[selected_col].iloc[-n_test:]
+    y_val = df[df.iloc[:,-1:].columns].iloc[-n_test:]
+    
+    df.drop('Return',axis=1,inplace=True)
+    
+
+  return X,y,X_val,y_val
 
 def calculate_aic(n, mse, num_params):
 	  aic = n * m.log(mse) + 2 * num_params
